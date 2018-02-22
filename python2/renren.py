6209cfd3b86e72b74439789a5fe3a93f71e9bcfc
Cecilia Chen <xcchen@xcchen-macbookpro.roam.corp.google.com>
2018-02-21 16:22:52 -0800

    Renren: Migrated to python3

--- python2/renren.py
+++ python2/renren.py
@@ -1,34 +1,19 @@
+#! /usr/lib/python  
 #-*- coding: utf-8 -*-  
-""" Renren status crawler.
 
-Examples:
-    $ python3 renren.py
-
-Todo:
-  * Move ``get_soup`` function to separate ``util`` module.
-
-"""
-
-from datetime import datetime
+import cookielib
 import json
 import re
+import requests
 import sys
 import urllib
+import urllib2
 
-from http.cookiejar import CookieJar
+from datetime import datetime
 from bs4 import BeautifulSoup
 
-DATETIME_INPUT_PATTERN = '%Y年%m月%d日 %H:%M'
+DATETIME_INPUT_PATTERN = '%Y年%m月%d日 %H:%M\xc2\xa0'
 DATETIME_OUTPUT_PATTERN = '%Y-%m-%d %H:%M'
-MAX_PAGE = -1
-
-
-def __get_soup(url, data=None):
-  encoded_data = urllib.parse.urlencode(data).encode("utf-8") if data else None
-  request = urllib.request.Request(url, encoded_data)
-  response = urllib.request.urlopen(request).read()
-  soup = BeautifulSoup(response, 'html.parser')
-  return soup
 
 
 class RenrenCrawler:
@@ -40,49 +25,50 @@ class RenrenCrawler:
     self.sid = ''
     self.status = []
     try:
-      self.cookie = CookieJar()
-      self.cookie_proc = urllib.request.HTTPCookieProcessor(self.cookie)
+      self.cookie = cookielib.CookieJar()
+      self.cookie_proc = urllib2.HTTPCookieProcessor(self.cookie)
     except:
       raise
     else:
-      opener = urllib.request.build_opener(self.cookie_proc)
-      urllib.request.install_opener(opener)
+      opener = urllib2.build_opener(self.cookie_proc)
+      urllib2.install_opener(opener)
 
   def login(self):
     url='http://3g.renren.com/login.do'
     data = {'email':self.email, 'password':self.password}
+    request = urllib2.Request(url, urllib.urlencode(data))
 
     try:
-      index = get_soup(url, data)
+      index = BeautifulSoup(urllib2.urlopen(request).read(), 'html.parser')
       href = str(index.select('.cur')[0].parent.contents[2]['href'])
       # Get user id.
       self.id = re.findall(r"\d{6,}", href)[0]
       # Get user sid.
       self.sid = re.findall(r"(?<=sid=).*?(?=&)", href)[0]
       return True
-    except Exception as err:
-      print('Login failed: ', err)
+
+    except Exception, e:
+      print 'Login failed: ', e.message
       return False
 
-  def get_status(self, max_page=-1):
+  def get_status(self):
     # Get status url.
     url = 'http://3g.renren.com/profile.do'
     data = {'id':str(self.id), 'sid':self.sid}
-    profile_soup = get_soup(url, data)
-    url = profile_soup.select('.sec')[3].find_all('a')[3]['href']
+    request = urllib2.Request(url, urllib.urlencode(data))
+    profile = BeautifulSoup(urllib2.urlopen(request).read(), 'html.parser')
+    url = profile.select('.sec')[3].find_all('a')[3]['href']
     
     # Get total page count.
-    status_soup = get_soup(url)
-    page_content = str(status_soup.select('.gray')[0].contents)
+    request = urllib2.Request(url)
+    status_page = BeautifulSoup(urllib2.urlopen(request).read(), 'html.parser')
+    page_content = str(status_page.select('.gray')[0].contents)
     total_page = int(re.findall(r"(?<=/)\d+(?=[^\d])", page_content)[0])
-
-    if max_page < total_page and max_page > 0:
-      total_page = max_page
 
     # Iterate pages.
     current_page = 1
     while current_page <= total_page:
-      status_list = status_soup.select('.list')[0].find_all('div')
+      status_list = status_page.select('.list')[0].find_all('div')
       for status in status_list:
         if status.select('.time'):
           if not status.select('.forward'):
@@ -92,16 +78,17 @@ class RenrenCrawler:
             obj['datetime'] = time.strftime(DATETIME_OUTPUT_PATTERN)
             obj['content'] = (status.a.next_element).strip()
             self.status.append(obj)
-            print(obj['datetime'])
-            print(obj['content'])
-            print('')
+            print obj['datetime']
+            print obj['content'].encode('utf-8')
+            print ''
       current_page += 1
       if current_page > total_page:
         break
 
       # Get the next page.
       url = str(status_page.select(".l")[0].a['href'])
-      status_soup = get_soup(url)
+      req = urllib2.Request(url)
+      status_page = BeautifulSoup(urllib2.urlopen(req).read(), 'html.parser')
 
   def write_json(self, filepath):
     with open(filepath, 'w') as file:
@@ -112,12 +99,18 @@ class RenrenCrawler:
       lines = ["{}\t{}\n".format(a['datetime'], a['content']) for a in self.status]
       file.writelines(lines)
 
+  def write_csv(self, filepath):
+    pass
+
 
 if __name__ == '__main__':
-  email = input("Input account/email: ")
-  password = input("Input password: ")
+  email = raw_input("Input account/email: ")
+  password = raw_input("Input password: ")
+  reload(sys)
+  sys.setdefaultencoding('utf-8')
   crawler = RenrenCrawler(email, password)
   if crawler.login():
-    crawler.get_status(MAX_PAGE)
-    print('Writing result to output.txt...')
+    crawler.get_status()
+    print "Writing result to output.txt..."
     crawler.write_txt('output.txt')
+ 
